92b0c94319b102e9f17a4e734a2a6a36ca61fd7d
michalkryjan <m.kryjan@iamcodee.co>
2024-02-17 14:44:58 +0100

    - add basic global meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@ export default defineNuxtConfig({
   devtools: { 
     enabled: true 
   app: {
+    head: {
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'format-detection', content: 'telephone=no' }
+      ],
       link: [
         { rel: 'icon', type: 'image/png', href: '/favicon/favicon-32x32.png', sizes: '32x32' },
         { rel: 'icon', type: 'image/png', href: '/favicon/favicon-16x16.png', sizes: '16x16' }
