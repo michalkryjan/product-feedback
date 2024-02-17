0b9e6b31a0139483e50e4bfc2058be28f784dc9b
michalkryjan <m.kryjan@iamcodee.co>
2024-02-18 00:51:49 +0100

    - add component BaseIcon

---
+++ components/BaseIcon.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+interface IBaseIconProps {
+  name: string
+  filled?: boolean
+  fillContainer?: boolean
+}
+
+const props = withDefaults(defineProps<IBaseIconProps>(), {
+  filled: true,
+  fillContainer: true
+})
+
+const classes = computed(() => {
+  return [
+    '[&_svg]:!mb-0',
+    props.fillContainer ? '[&>svg]:!w-full [&>svg]:!h-full' : ''
+  ]
+})
+</script>
+
+<template>
+  <nuxt-icon
+    :name="name"
+    :filled="filled"
+    :class="classes" />
+</template>
