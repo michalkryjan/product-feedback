2430b5f08fc9815f745b9509ff6bc93eb07619d5
michalkryjan <m.kryjan@iamcodee.co>
2024-02-17 14:44:37 +0100

    - add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 export default defineNuxtConfig({
   devtools: { 
     enabled: true 
+  app: {
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon/favicon-32x32.png', sizes: '32x32' },
+        { rel: 'icon', type: 'image/png', href: '/favicon/favicon-16x16.png', sizes: '16x16' }
+      ]
+    }
   }
 })
