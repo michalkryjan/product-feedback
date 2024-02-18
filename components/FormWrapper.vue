09b8cca2f910b473658a015cf52ccc44bbad6436
michalkryjan <m.kryjan@iamcodee.co>
2024-02-18 14:07:28 +0100

    - add default form wrapper

---
+++ components/FormWrapper.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+interface IFormWrapperProps {
+  onSubmit: (e?: Event | undefined) => (void | Promise<void> | Promise<Promise<void> | undefined>)
+}
+
+const props = defineProps<IFormWrapperProps>()
+
+const formWrapper = ref<HTMLFormElement | null>(null)
+
+const classes = computed(() => {
+  return [
+    'flex flex-col flex-nowrap'
+  ]
+})
+</script>
+
+<template>
+  <form
+    ref="formWrapper"
+    method=""
+    action=""
+    @submit="onSubmit">
+    <div :class="classes">
+      <slot name="main" />
+    </div>
+
+    <slot name="footer" />
+  </form>
+</template>
