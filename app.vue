e21ee06d38a19728707060c1bbaaaf0aafa97b03
michalkryjan <m.kryjan@iamcodee.co>
2024-02-20 18:52:33 +0100

    - update main app

--- app.vue
+++ app.vue
@@ -3,15 +3,15 @@
     lang="en"
     class="h-full">
     <Body class="m-0 h-full min-h-full min-w-320 w-full">
-      <nuxt-layout>
-        <nuxt-page />
-      </nuxt-layout>
+      <nuxt-page />
     </Body>
   </Html>
 </template>
 
-<style lang="postcss">
+<style lang="css">
 #__nuxt {
-  @apply h-full flex flex-col;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
 }
 </style>
