7cef520ff711334d1702f7f7a53f34d4b1b559a9
michalkryjan <m.kryjan@iamcodee.co>
2024-02-20 18:54:03 +0100

    - add prototype of index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,30 @@
 <script setup lang="ts">
-definePageMeta({ layout: 'board-with-sidebar' })
+import type { Content } from 'types/content'
+
+const { data: globalContent } = await useAsyncData('global',
+  () => queryContent<Content.Global>('global').findOne()
+)
+const { data: pageContent } = await useAsyncData('homepage',
+  () => queryContent<Content.Homepage>('homepage').findOne()
+)
+
+useHead({ title: `${pageContent.value?.title ?? globalContent.value?.title}`, })
 </script>
 
 <template>
-  <div />
+  <nuxt-layout name="board-with-sidebar">
+    <template #sidebar>
+      <main-menu-product-info
+        v-if="pageContent?.productInfo"
+        :data="pageContent.productInfo" />
+
+      <main-menu-suggestions-filters
+        v-if="pageContent?.filters"
+        :data="pageContent.filters" />
+
+      <main-menu-roadmap-summary
+        v-if="pageContent?.roadmapSummary"
+        :data="pageContent.roadmapSummary" />
+    </template>
+  </nuxt-layout>
 </template>
