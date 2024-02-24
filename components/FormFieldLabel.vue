e027c6ab9f18ee413507fedc2192d9c5647a7947
michalkryjan <michal.kryjan@gmail.com>
2024-02-24 17:51:52 +0100

    - add form field label

---
+++ components/FormFieldLabel.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+interface IFormLabelProps {
+  tag?: 'label' | 'span'
+  value: string
+}
+
+const props = withDefaults(defineProps<IFormLabelProps>(), {
+  tag: 'label'
+})
+
+const {
+  formFieldWrapper
+} = useDependencyInjectionSchema()
+
+const fieldName = inject(formFieldWrapper.name, '')
+</script>
+
+<template>
+  <component
+    :is="tag"
+    :for="tag === 'label' ? fieldName : undefined" />
+</template>
