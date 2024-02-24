8aebdb9e12ded65a2127a13f7a5c1dffd3a8e53f
michalkryjan <m.kryjan@iamcodee.co>
2024-02-24 10:27:06 +0100

    - add padding top in layout board with sidebar

--- layouts/BoardWithSidebar.vue
+++ layouts/BoardWithSidebar.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <div class="grid grid-cols-[26rem_1fr] mx-auto max-w-[111rem] w-full gap-x-[3rem]">
+  <div class="grid grid-cols-[26rem_1fr] mx-auto max-w-[111rem] w-full gap-x-[3rem] pt-[9rem]">
     <div class="col-start-1 col-end-1">
       <slot name="sidebar" />
     </div>
