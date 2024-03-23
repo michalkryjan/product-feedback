9fdc4135e809b74c563cc2cfee8e2093d99e7a3e
michalkryjan <michal.kryjan@gmail.com>
2024-03-23 19:47:58 +0100

    - add component list summary

---
+++ components/ListSummary.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { Data } from 'types/data'
+
+interface IBaseListSummaryProps {
+  items: Data.Components.ListSummaryItem[]
+}
+
+const props = defineProps<IBaseListSummaryProps>()
+</script>
+
+<template>
+  <base-list tag="ul">
+    <list-summary-item
+      v-for="(item, index) in items"
+      :key="index"
+      :data="item" />
+  </base-list>
+</template>
---
+++ components/ListSummaryItem.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import type { Data } from 'types/data'
+
+interface IListSummaryItemProps {
+  data: Data.Components.ListSummaryItem
+}
+
+const props = defineProps<IListSummaryItemProps>()
+
+const configMarker: Record<Data.Components.ListSummaryItem['markerColor'], string> = {
+  orange: 'text-orange-1',
+  purple: 'text-purple-1',
+  lightBlue: 'text-lightBlue-1'
+}
+
+const classes = computed(() => {
+  return [
+    'inline-flex justify-between gap-x-20 w-full',
+    configMarker[props.data.markerColor]
+  ]
+})
+</script>
+
+<template>
+  <li :class="classes">
+    <base-text
+      tag="span"
+      typography="body-1"
+      color="gray"
+      :value="data.label" />
+
+    <base-text
+      tag="strong"
+      typography="body-1"
+      color="gray"
+      :value="data.count + ''" />
+  </li>
+</template>
