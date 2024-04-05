8ab9b2a8d5c735fcdc711cf5d9ee2d75ace1bc9f
michalkryjan <michal.kryjan@gmail.com>
2024-04-05 22:21:57 +0200

    - add base header component

---
+++ components/base/Header.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+interface IBaseHeaderProps {
+  variant: '1' | '2' | '3'
+}
+
+const props = defineProps<IBaseHeaderProps>()
+
+const configVariant: Record<NonNullable<IBaseHeaderProps['variant']>, string> = {
+  1: 'mb-40',
+  2: 'mb-32',
+  3: 'mb-24'
+}
+</script>
+
+<template>
+  <header :class="configVariant[variant]">
+    <slot />
+  </header>
+</template>
