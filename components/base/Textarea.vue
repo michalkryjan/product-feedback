fa09ceffbb8578f4bbafe2da3412f786cc8a396a
michalkryjan <michal.kryjan@gmail.com>
2024-04-06 16:53:22 +0200

    - improve transition in forms fields

--- components/base/Textarea.vue
+++ components/base/Textarea.vue
@@ -19,7 +19,7 @@ const props = withDefaults(defineProps<IBaseTextareaProps>(), {
 const emit = defineEmits<IBaseInputEmits>()
 
 const configVariant: Record<NonNullable<IBaseTextareaProps['variant']>, string> = {
-  1: 'w-full h-96 px-24 py-16 text-15 rounded-5 text-navy-1 border border-gray-3 bg-gray-3 duration-200 focus:border-blue-1 outline-none'
+  1: 'w-full h-96 px-24 py-16 text-15 rounded-5 text-navy-1 border border-gray-3 bg-gray-3 transition-colors duration-200 focus:border-blue-1 outline-none'
 }
 
 const classes = computed(() => {
