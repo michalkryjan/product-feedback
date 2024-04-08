cb04acdf86543228748fde0cb7d081f8a350dd57
michalkryjan <michal.kryjan@gmail.com>
2024-04-08 17:20:17 +0200

    - add feedback list default

---
+++ components/FeedbackListDefault.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+interface ICardFeedbackListProps {
+  items: Feedback[]
+}
+
+const props = defineProps<ICardFeedbackListProps>()
+</script>
+
+<template>
+  <card-feedback
+    v-for="item in items"
+    :key="item.id"
+    :data="item"
+    class="mb-20 last:mb-0" />
+</template>
