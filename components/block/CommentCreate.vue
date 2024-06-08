a604a6ca4eae3b887cb4ed684c1fa98aee92bce5
michalkryjan <michal.kryjan@gmail.com>
2024-06-08 18:55:11 +0200

    - add components BlockCommentCreate and FormCommentCreate

--- components/block/CommentCreate.vue
+++ components/block/CommentCreate.vue
@@ -3,5 +3,14 @@
 </script>
 
 <template>
-  <form-comment-create />
+  <ui-card>
+    <base-headline
+      :level="2"
+      typography="title-3"
+      color="navy">
+      <span>Add comment</span>
+    </base-headline>
+
+    <form-comment-create class="mt-24" />
+  </ui-card>
 </template>
--- components/form/comment/Create.vue
+++ components/form/comment/Create.vue
@@ -1,8 +1,80 @@
 <script setup lang="ts">
+import { useForm } from 'vee-validate'
+
+interface IFormCommentCreateValues extends Pick<IComment, 'content'> {}
+
+interface IFormCommentCreateEmits {
+  (e: 'success'): void
+}
+
+const emit = defineEmits<IFormCommentCreateEmits>()
+
+const charsLimit = 250
+const charsCount = ref<number>(0)
+const submitDisabled = ref<boolean>(false)
+
+const {
+  handleSubmit,
+  values
+} = useForm<IFormCommentCreateValues>({
+  validationSchema: {
+    content: ''
+  },
+  initialValues: {
+    content: undefined
+  }
+})
+
+const onFormSubmit = handleSubmit(async (values: IFormCommentCreateValues) => {
+  await postFormData(values)
+})
+
+async function postFormData (values: IFormCommentCreateValues) {
+  submitDisabled.value = true
+
+  try {
+    // API call
+    console.log(values)
+    emit('success')
+  } catch (e) {
+    console.log(e)
+  }
+
+  submitDisabled.value = false
+}
+
+watch(() => values.content, () => {
+  charsCount.value = values.content.length
+})
 </script>
 
 <template>
-  <ui-card>
-    <div />
-  </ui-card>
+  <base-form
+    gap-size="s2"
+    :on-submit="onFormSubmit">
+    <template #main>
+      <form-field-wrapper name="content">
+        <template #field="{ id, errors, handleChange }">
+          <base-textarea
+            :id="id"
+            @update:model-value="handleChange" />
+        </template>
+      </form-field-wrapper>
+    </template>
+
+    <template #footer>
+      <div class="inline-flex justify-between items-center gap-x-32 w-full">
+        <base-content
+          typography="text-body-2"
+          color="gray">
+          {{ (charsLimit - charsCount) + ' characters left' }}
+        </base-content>
+        <base-button
+          text="Post comment"
+          size="s2"
+          theme="purple"
+          label-color="white" />
+      </div>
+    </template>
+  </base-form>
 </template>
